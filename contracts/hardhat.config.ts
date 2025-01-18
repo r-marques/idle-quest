049fed1d6890cc35fb93ed077316e586606a463e
Rodolphe Marques <marques.rodolphe@gmail.com>
2025-01-18 18:34:50 +0100

    revert hardhat configuration

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,11 +1,8 @@
 import { HardhatUserConfig, vars } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 
-const INFURA_API_KEY = vars.get('INFURA_API_KEY', 'default-for-CI');
-const BASE_SEPOLIA_PRIVATE_KEY = vars.get(
-  'BASE_SEPOLIA_PRIVATE_KEY',
-  'default-for-CI',
-);
+const INFURA_API_KEY = vars.get('INFURA_API_KEY');
+const BASE_SEPOLIA_PRIVATE_KEY = vars.get('BASE_SEPOLIA_PRIVATE_KEY');
 
 const config: HardhatUserConfig = {
   solidity: '0.8.28',
