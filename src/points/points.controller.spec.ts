16630628024803b440c923233bdc5d0af9e5ed1c
Rodolphe Marques <marques.rodolphe@gmail.com>
2025-01-18 14:59:40 +0100

    fix tests

--- src/points/points.controller.spec.ts
+++ src/points/points.controller.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PointsController } from './points.controller';
+import { Web3Service } from '../web3/web3.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 describe('PointsController', () => {
   let controller: PointsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot({ envFilePath: '.env.development' })],
       controllers: [PointsController],
+      providers: [Web3Service, ConfigService],
     }).compile();
 
     controller = module.get<PointsController>(PointsController);
