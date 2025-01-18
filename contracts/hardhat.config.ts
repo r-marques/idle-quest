2a8700cbb3e0dd5b0b9911fa115da11c74cee64f
Rodolphe Marques <marques.rodolphe@gmail.com>
2025-01-18 19:00:04 +0100

    fix contracts workflow

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,13 @@
 import { HardhatUserConfig, vars } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 
-const INFURA_API_KEY = vars.get('INFURA_API_KEY');
-const BASE_SEPOLIA_PRIVATE_KEY = vars.get('BASE_SEPOLIA_PRIVATE_KEY');
+// Random defaults so that hardhat does not fail when developing locally
+// and the vars are not set
+const INFURA_API_KEY = vars.get('INFURA_API_KEY', 'apikey');
+const BASE_SEPOLIA_PRIVATE_KEY = vars.get(
+  'BASE_SEPOLIA_PRIVATE_KEY',
+  '0xadf600bca523b58ffda76eda51c29c378451a9803f506361a1f906da4df1de78',
+);
 
 const config: HardhatUserConfig = {
   solidity: '0.8.28',
