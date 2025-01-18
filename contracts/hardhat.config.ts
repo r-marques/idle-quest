81436f16b4e51c858126c5c2f749319572aaa47f
Rodolphe Marques <marques.rodolphe@gmail.com>
2025-01-18 15:39:40 +0100

    add base-sepolia network to hardhat config

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,17 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig, vars } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+
+const INFURA_API_KEY = vars.get('INFURA_API_KEY');
+const BASE_SEPOLIA_PRIVATE_KEY = vars.get('BASE_SEPOLIA_PRIVATE_KEY');
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: '0.8.28',
+  networks: {
+    'base-sepolia': {
+      url: `https://base-sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [BASE_SEPOLIA_PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
